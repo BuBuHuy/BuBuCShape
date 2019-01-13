75af34bcc73b0cdd9b5015ac712a2b35a6eea5b3
BuBuHuy <luuvanhuy2012@gmail.com>
2019-01-13 19:57:39 +0700

    Winform Basic

---
+++ DentalPayMentApp/DentalPayMentApp/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace DentalPayMentApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.cb1 = new System.Windows.Forms.CheckBox();
+            this.cb2 = new System.Windows.Forms.CheckBox();
+            this.cb3 = new System.Windows.Forms.CheckBox();
+            this.cv = new System.Windows.Forms.Label();
+            this.yt = new System.Windows.Forms.Label();
+            this.cr = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cbb = new System.Windows.Forms.ComboBox();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.btnTinh = new System.Windows.Forms.Button();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AllowDrop = true;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(285, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Dental Payment Form";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(95, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ten Khach Hang";
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(200, 64);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(316, 20);
+            this.txt1.TabIndex = 2;
+            // 
+            // cb1
+            // 
+            this.cb1.AutoSize = true;
+            this.cb1.Location = new System.Drawing.Point(67, 109);
+            this.cb1.Name = "cb1";
+            this.cb1.Size = new System.Drawing.Size(63, 17);
+            this.cb1.TabIndex = 3;
+            this.cb1.Text = "Cao Voi";
+            this.cb1.UseVisualStyleBackColor = true;
+            // 
+            // cb2
+            // 
+            this.cb2.AutoSize = true;
+            this.cb2.Location = new System.Drawing.Point(67, 155);
+            this.cb2.Name = "cb2";
+            this.cb2.Size = new System.Drawing.Size(71, 17);
+            this.cb2.TabIndex = 4;
+            this.cb2.Text = "tay Trang";
+            this.cb2.UseVisualStyleBackColor = true;
+            // 
+            // cb3
+            // 
+            this.cb3.AutoSize = true;
+            this.cb3.Location = new System.Drawing.Point(67, 205);
+            this.cb3.Name = "cb3";
+            this.cb3.Size = new System.Drawing.Size(80, 17);
+            this.cb3.TabIndex = 5;
+            this.cb3.Text = "Chup Rang";
+            this.cb3.UseVisualStyleBackColor = true;
+            // 
+            // cv
+            // 
+            this.cv.AutoSize = true;
+            this.cv.Location = new System.Drawing.Point(344, 113);
+            this.cv.Name = "cv";
+            this.cv.Size = new System.Drawing.Size(25, 13);
+            this.cv.TabIndex = 6;
+            this.cv.Text = "100";
+            // 
+            // yt
+            // 
+            this.yt.AutoSize = true;
+            this.yt.Location = new System.Drawing.Point(344, 155);
+            this.yt.Name = "yt";
+            this.yt.Size = new System.Drawing.Size(25, 13);
+            this.yt.TabIndex = 7;
+            this.yt.Text = "120";
+            // 
+            // cr
+            // 
+            this.cr.AutoSize = true;
+            this.cr.Location = new System.Drawing.Point(350, 205);
+            this.cr.Name = "cr";
+            this.cr.Size = new System.Drawing.Size(19, 13);
+            this.cr.TabIndex = 8;
+            this.cr.Text = "80";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(70, 266);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(305, 303);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "label7";
+            // 
+            // cbb
+            // 
+            this.cbb.FormattingEnabled = true;
+            this.cbb.Items.AddRange(new object[] {
+            "1 ",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6"});
+            this.cbb.Location = new System.Drawing.Point(128, 258);
+            this.cbb.Name = "cbb";
+            this.cbb.Size = new System.Drawing.Size(121, 21);
+            this.cbb.TabIndex = 11;
+            this.cbb.SelectedIndexChanged += new System.EventHandler(this.cbb_SelectedIndexChanged);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(73, 325);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(75, 23);
+            this.btnThoat.TabIndex = 12;
+            this.btnThoat.Text = "Thoat";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // btnTinh
+            // 
+            this.btnTinh.Location = new System.Drawing.Point(403, 334);
+            this.btnTinh.Name = "btnTinh";
+            this.btnTinh.Size = new System.Drawing.Size(75, 23);
+            this.btnTinh.TabIndex = 13;
+            this.btnTinh.Text = "Tinh";
+            this.btnTinh.UseVisualStyleBackColor = true;
+            this.btnTinh.Click += new System.EventHandler(this.btnTinh_Click);
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(378, 300);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(100, 20);
+            this.txt2.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(720, 369);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.btnTinh);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.cbb);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cr);
+            this.Controls.Add(this.yt);
+            this.Controls.Add(this.cv);
+            this.Controls.Add(this.cb3);
+            this.Controls.Add(this.cb2);
+            this.Controls.Add(this.cb1);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.CheckBox cb1;
+        private System.Windows.Forms.CheckBox cb2;
+        private System.Windows.Forms.CheckBox cb3;
+        private System.Windows.Forms.Label cv;
+        private System.Windows.Forms.Label yt;
+        private System.Windows.Forms.Label cr;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox cbb;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Button btnTinh;
+        private System.Windows.Forms.TextBox txt2;
+    }
+}
+
---
+++ DentalPayMentApp/DentalPayMentApp/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DentalPayMentApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            Dispose();  
+        }
+        public int TinhTien()
+        {
+            //int a, b, c;
+            int total = 0;
+            int so =0;
+            CheckBox ck = new CheckBox();
+            if (cb1.Checked == true) total += Int32.Parse(cv.Text);
+            if (cb2.Checked == true) total += Int32.Parse(yt.Text);
+            if (cb3.Checked == true) total += Int32.Parse(cr.Text);
+            try
+            {
+                so = Convert.ToInt32(cbb.Items[cbb.SelectedIndex].ToString());
+            }
+            catch
+            {
+                MessageBox.Show("Dien so Rang");
+            }
+            return total * so;
+        }
+        private void btnTinh_Click(object sender, EventArgs e)
+        {
+            if(txt1.Text == null)
+            {
+                MessageBox.Show("Dien ten Khach Hang");
+
+                return;
+            }
+            txt2.Text = string.Concat(TinhTien()) ;
+            MessageBox.Show("Tien cua quy khach la :" + txt2.Text);
+        }
+
+        private void cbb_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
